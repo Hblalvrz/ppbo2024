80ccf30bb92d706b8d0d54e26afe298dcd102bf7
Hblalvrz <H1101231016@student.untan.ac.id>
2024-09-02 20:59:43 +0700

    Praktikum 2B - Menggunakan Fungsi Bawaan

--- pertama.php
+++ pertama.php
@@ -1,5 +1,5 @@
 <?php
 date_default_timezone_set("Asia/Jakarta");
 $nama = readline('Masukkan nama Anda: ');
-$waktu = date('H');
+$waktu = date('H:i'); // untuk menampilkan jam dan menit maka yang sebelumnya hanya 'H' diganti menjadi 'H:i'
 echo "Halo {$nama} sekarang pukul {$waktu}\n";
