65ff0b5933bba317f736ea59bcace07596ca1246
Hblalvrz <H1101231016@student.untan.ac.id>
2024-09-04 17:53:07 +0700

    Praktikum 4B - Membuat Atribut

--- ketiga.php
+++ ketiga.php
@@ -3,45 +3,71 @@
 class Lingkaran
 {
     const PHI = 3.14;
+    public $jari_jari;
 
-
-    public function luas($jari_jari) : float {
-        return self::PHI*$jari_jari*$jari_jari;
+    public function luas() : float {
+        return self::PHI * pow($this->jari_jari, 2);
     }
 
-
-    public function keliling($jari_jari) : float {
-        return 2*self::PHI*$jari_jari;
+    public function keliling() : float {
+        return 2 * self::PHI * $this->jari_jari;
     }
 }
-
 
 class Bola
 {
     const PHI = 3.14;
-    public function volume($jari_jari) : float {
-        return (4/3)*self::PHI*pow($jari_jari,3);
+    public $jari_jari;
+
+    public function volume() : float {
+        return (4/3) * self::PHI * pow($this->jari_jari, 3);
     }
 }
-
 
 class Tabung
 {
     const PHI = 3.14;
-    public function volume($jari_jari,$tinggi) : float {
-        return self::PHI*pow($jari_jari,2)*$tinggi;
+    public $jari_jari;
+    public $tinggi;
+
+    public function volume() : float {
+        return self::PHI * pow($this->jari_jari, 2) * $this->tinggi;
     }
 }
-
 
 class Kerucut
 {
     const PHI = 3.14;
-    public function volume($jari_jari,$tinggi) : float {
-        return (1/3)*self::PHI*pow($jari_jari,2)*$tinggi;
+    public $jari_jari;
+    public $tinggi;
+
+    public function volume() : float {
+        return (1/3) * self::PHI * pow($this->jari_jari, 2) * $this->tinggi;
     }
 }
 
 $nasi_tumpeng = new Kerucut();
-$volume_nasi_tumpeng = $nasi_tumpeng->volume(4,10);
-echo "Volume nasi tumpeng adalah {$volume_nasi_tumpeng} cm^3";
+$nasi_tumpeng -> jari_jari = 5;
+$nasi_tumpeng -> tinggi = 8;
+$hasil = $nasi_tumpeng -> volume();
+echo "Volume nasi tumpeng adalah: {$hasil} cm^3\n";
+
+$lingkaran = new Lingkaran();
+$lingkaran -> jari_jari = 5;
+$hasil_luas = $lingkaran -> luas();
+$hasil_keliling = $lingkaran -> keliling();
+echo "Luas lingkaran adalah: {$hasil_luas} cm^2\n";
+echo "Keliling lingkaran adalah: {$hasil_keliling} cm\n";
+
+$bola = new Bola();
+$bola -> jari_jari = 5;
+$hasil = $bola -> volume();
+echo "Volume bola adalah: {$hasil} cm^3\n";
+
+$tabung = new Tabung();
+$tabung -> jari_jari = 5;
+$tabung -> tinggi = 8;
+$hasil = $tabung -> volume();
+echo "Volume tabung adalah: {$hasil} cm^3\n";
+
+?>
