1eeab8c252364eec4f95ca173a22622580fb7250
Hblalvrz <H1101231016@student.untan.ac.id>
2024-09-26 21:43:55 +0700

    Praktikum 6Dx - Implementasi Enkapsulasi dan Inheritance

--- App/Model/Akademik/Dosen.php
+++ App/Model/Akademik/Dosen.php
@@ -3,12 +3,12 @@
 namespace App\Model\Akademik;
 
 class Dosen extends Pegawai {
-    public string $nidn;
+    private string $nidn;
 
     public function __construct(int $nip, string $nama, string $alamat, int $no_hp, string $nidn) {
         parent::__construct($nip, $nama, $alamat, $no_hp);
         $this->nidn = $nidn;
-    }
+    }  
 
     public function mengajar(): void {
         echo "{$this->nama} sedang mengajar perkuliahan.\n";
--- sebelas.php
+++ sebelas.php
@@ -5,4 +5,5 @@ require_once 'vendor/autoload.php';
 use App\Model\Akademik\Dosen;
 
 $dian = new Dosen(198411132015041001, "Dian Prawira", "Jln Purnama", 62111111, "0013118405");
+$dian -> getNoHp();
 $dian->mengajar();
